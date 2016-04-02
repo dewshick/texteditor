4948ce54ee2293808a46b9d4086849990bbc9bb7
Andrey Vyatkin <avyatkin@spbtv.com>
2016-04-02 20:43:32 +0300

    add js support

--- src/main/java/syntax/document/CodeDocumentFactory.java
+++ src/main/java/syntax/document/CodeDocumentFactory.java
@@ -5,6 +5,7 @@ import syntax.SyntaxColorRule;
 import syntax.brackets.BracketIndex;
 
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
@@ -12,24 +13,43 @@ import java.util.function.Function;
 /**
  * Created by avyatkin on 02/04/16.
  */
+
 public class CodeDocumentFactory {
+
     static final String[] JAVA_KEYWORDS = {"'abstract'","'assert'","'boolean'","'break'","'byte'","'case'","'catch'","'char'","'class'","'const'","'continue'","'default'","'do'","'double'","'else'","'enum'","'extends'","'final'","'finally'","'float'","'for'","'if'","'goto'","'implements'","'import'","'instanceof'","'int'","'interface'","'long'","'native'","'new'","'package'","'private'","'protected'","'public'","'return'","'short'","'static'","'strictfp'","'super'","'switch'","'synchronized'","'this'","'throw'","'throws'","'transient'","'try'","'void'","'volatile'","'while'"};
-
     public static CodeDocument forJava() {
-        List<SyntaxColorRule> colorRules = Arrays.asList(
-            new SyntaxColorRule(Color.BLUE, JAVA_KEYWORDS),
-            new SyntaxColorRule(Color.ORANGE, "Identifier"),
-            new SyntaxColorRule(Color.GRAY, "LINE_COMMENT", "COMMENT"));
-
-        List<Function<String, BracketIndex>> indexFactories = Arrays.asList(
-            code -> new BracketIndex("'('", "')'", LexerWrapper.javaLexer(code)),
-            code -> new BracketIndex("'{'", "'}'", LexerWrapper.javaLexer(code))
-        );
-
-        return new CodeDocument(colorRules, indexFactories);
+        return documentForSpecificLanguage(
+                JAVA_KEYWORDS,
+                "Identifier",
+                new String[]{ "LINE_COMMENT", "COMMENT" },
+                new String[][]{ { "'('", "')'" }, { "'{'", "'}'" }, {"'['", "']'"} },
+                LexerWrapper::jsLexer);
     }
-//
-//    public static CodeDocument forJs() {
-//        new CodeDocument();
-//    }
+
+//    for some reason, function keyword in js is recognized as identifier
+    static final String[] JS_KEYWORDS = {"'break'","'do'","'instanceof'","'typeof'","'case'","'else'","'new'","'var'","'catch'","'finally'","'return'","'void'","'continue'","'for'","'switch'","'while'","'debugger'","'function'","'this'","'with'","'default'","'if'","'throw'","'delete'","'in'","'try'","'class'","'enum'","'extends'","'super'","'const'","'export'","'import'","'implements'","'let'","'private'","'public'","'interface'","'package'","'protected'","'static'","'yield'"};
+    public static CodeDocument forJs() {
+        return documentForSpecificLanguage(
+                JS_KEYWORDS,
+                "Identifier",
+                new String[]{ "MultiLineComment", "SingleLineComment" },
+                new String[][]{ { "'('", "')'" }, { "'{'", "'}'" }, {"'['", "']'"} },
+                LexerWrapper::jsLexer);
+    }
+
+    private static CodeDocument documentForSpecificLanguage(String[] keywordTokens,
+                                                            String identifier,
+                                                            String[] commentTokens,
+                                                            String[][] bracketPairs,
+                                                            Function<String,LexerWrapper> lexerFactory) {
+        List<SyntaxColorRule> colorRules = Arrays.asList(
+                new SyntaxColorRule(Color.BLUE, keywordTokens),
+                new SyntaxColorRule(Color.ORANGE, identifier),
+                new SyntaxColorRule(Color.GRAY, commentTokens));
+        List<Function<String, BracketIndex>> bracketIndexFactories = new ArrayList<>();
+        for (String[] bracketTokenPair : bracketPairs)
+            bracketIndexFactories.add(code ->
+                    new BracketIndex(bracketTokenPair[0],bracketTokenPair[1], lexerFactory.apply(code)));
+        return new CodeDocument(colorRules, bracketIndexFactories);
+    }
 }
