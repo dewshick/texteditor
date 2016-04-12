fe6ac1c9ec33f740def142c01665e979d0fb95a2
Andrey Vyatkin <avyatkin@spbtv.com>
2016-04-13 00:21:13 +0300

    implement dumbest possible caret

--- src/main/java/gui/EditorTextBox.java
+++ src/main/java/gui/EditorTextBox.java
@@ -2,16 +2,18 @@ package gui;
 
 import org.apache.commons.collections4.list.TreeList;
 
+import javax.accessibility.Accessible;
 import javax.swing.*;
 import javax.swing.text.Document;
 import java.awt.*;
+import java.awt.event.*;
 import java.util.Arrays;
 import java.util.List;
 
 /**
  * Created by avyatkin on 06/04/16.
  */
-public class EditorTextBox extends JComponent implements Scrollable // , Accessible
+public class EditorTextBox extends JComponent implements Scrollable, Accessible
 {
     String text;
     List<String> lines;
@@ -21,9 +23,14 @@ public class EditorTextBox extends JComponent implements Scrollable // , Accessi
     public EditorTextBox(Document doc) {
         text = "";
         lines = new TreeList<>();
+        lines.add("");
         editable = true;
         caret = new Caret();
         updatePreferredSize();
+        grabFocus();
+        setDoubleBuffered(true);
+        addFocusRelatedListeners();
+        addCaretMovementsListeners();
     }
 
     public String getText() { return text; }
@@ -110,6 +117,7 @@ public class EditorTextBox extends JComponent implements Scrollable // , Accessi
     }
 
     private void updatePreferredSize() {
+//        TODO: do not scan all the every time, store them in heap or hash by their length or something
         int width = lines.stream().map(this::stringWidth).reduce(0, Integer::max);
         int height = lines.size() * fontHeight();
         setPreferredSize(new Dimension(width, height));
@@ -135,33 +143,98 @@ public class EditorTextBox extends JComponent implements Scrollable // , Accessi
         }
     }
 
+    /**
+     * Focus
+     */
+
+    private void addFocusRelatedListeners() {
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                requestFocusInWindow();
+            }
+        });
+    }
+
+    /**
+     * Caret
+     */
+
+    private int lastLine() { return lines.size() - 1; }
+
+    enum CaretDirection { UP, DOWN, LEFT, RIGHT }
+
+    private void addCaretMovementsListeners() {
+        addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                switch (e.getKeyCode()) {
+                    case KeyEvent.VK_UP:
+                        caret.move(CaretDirection.UP);
+                        break;
+                    case KeyEvent.VK_DOWN:
+                        caret.move(CaretDirection.DOWN);
+                        break;
+                    case KeyEvent.VK_LEFT:
+                        caret.move(CaretDirection.LEFT);
+                        break;
+                    case KeyEvent.VK_RIGHT:
+                        caret.move(CaretDirection.RIGHT);
+                        break;
+                }
+            }
+        });
+    }
+
     class Caret {
-        Caret() {
-            relativePosition = new Point(0, 0);
-        }
+        Caret() { relativePosition = new Point(0, 0); }
 
-        private Point getAbsolutePosition() {
-            return absoluteCoords(relativePosition);
-        }
+        private Point getAbsolutePosition() { return absoluteCoords(relativePosition); }
 
-        public void setRelativePosition(Point relativePosition) {
-            this.relativePosition = relativePosition;
-        }
+        public void setRelativePosition(Point relativePosition) { this.relativePosition = relativePosition; }
 
         Point relativePosition;
 
-        Rectangle caretRect() { return new Rectangle(getAbsolutePosition().x, getAbsolutePosition().y, 2, fontHeight()); }
+        Rectangle caretRect() {
+            int yCoord = getAbsolutePosition().y + (fontHeight() - fontMetrics().getAscent());
+            return new Rectangle(getAbsolutePosition().x, yCoord, 2, fontHeight());
+        }
 
         void renderCaret(Graphics g) {
             g.fillRect(caretRect().x, caretRect().y, caretRect().width, caretRect().height);
         }
 
-        void replace(Point place) {
+        void move(CaretDirection direction) {
+            Point previousPosition = (Point)relativePosition.clone();
+            switch (direction) {
+                case UP: verticalMove(-1);
+                    break;
+                case DOWN: verticalMove(1);
+                    break;
+                case LEFT: horizontalMove(-1);
+                    break;
+                case RIGHT: horizontalMove(1);
+                    break;
+            }
+            if (previousPosition != relativePosition) repaint();
+        }
 
+        private void verticalMove(int direction) {
+            int newY = Math.min(Math.max(relativePosition.y + direction, 0), lastLine());
+            if (newY == relativePosition.y) return;
+            int newX = Math.min(lines.get(newY).length(), relativePosition.x);
+            setRelativePosition(new Point(newX, newY));
+        }
+
+        private void horizontalMove(int direction) {
+            String currentLine = lines.get(relativePosition.y);
+            int newX = Math.min(Math.max(0, relativePosition.x+ direction), currentLine.length());
+            if (newX != relativePosition.x)
+                setRelativePosition(new Point(newX, relativePosition.y));
         }
     }
+
     /**
-     *
      *
      */
 }
